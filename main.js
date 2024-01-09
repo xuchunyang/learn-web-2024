7852d534a18ceaa6329708a9371a605266f4d0e2
Xu Chunyang <xuchunyang56@gmail.com>
2024-01-09 22:58:26 +0800

    Add pages index

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
+const pages = JSON.parse(import.meta.env.VITE_PAGES);
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+document.querySelector("#app").innerHTML = `
+<main>
+    <h1>Learn Web Development 2024</h1>
+    <ol>
+        ${pages
+    .map((page) => `<li><a href="/${page}/">${page}</a></li>`)
+    .join("\n")}
+</ol>
+</main>
 
-setupCounter(document.querySelector('#counter'))
+<footer style="font-size: 0.875rem; color: #666">
+<details>
+    <summary>
+        最近一次提交于 ${new Date(import.meta.env.VITE_GIT_COMMIT_DATE * 1000).toLocaleString()} : <a href="${import.meta.env.VITE_GIT_COMMIT_URL}">${import.meta.env.VITE_GIT_COMMIT_MESSAGE}</a>
+    </summary>
+    <pre>${import.meta.env.VITE_GIT_COMMIT_LOG}</pre>
+</details>
+</footer>
+`;
---
+++ vite.config.js
@@ -0,0 +1,44 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import { readdirSync, statSync } from "fs";
+import { execSync } from "node:child_process";
+
+const pages = readdirSync(__dirname)
+  .filter((file) => statSync(file).isDirectory())
+  .filter((dir) => /^\d{2}-\d{2}-.*/.test(dir));
+
+const input = pages.reduce((acc, page) => {
+  acc[page] = resolve(__dirname, page, "index.html");
+  return acc;
+}, {});
+
+process.env.VITE_PAGES = JSON.stringify(pages);
+
+const [hash, message, date] = execSync('git log -1 --format="%h%n%s%n%at"')
+  .toString()
+  .trim()
+  .split("\n");
+process.env.VITE_GIT_COMMIT_HASH = hash;
+process.env.VITE_GIT_COMMIT_MESSAGE = message;
+process.env.VITE_GIT_COMMIT_DATE = date;
+process.env.VITE_GIT_COMMIT_URL = `https://github.com/xuchunyang/learn-web-2024/commit/${hash}`;
+
+process.env.VITE_GIT_COMMIT_LOG = execSync("git log -1 --stat")
+  .toString()
+  .trim();
+
+process.env.VITE_GIT_TOTAL_COMMITS = execSync("git rev-list HEAD --count")
+  .toString()
+  .trim();
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        ...input,
+      },
+    },
+  },
+});
