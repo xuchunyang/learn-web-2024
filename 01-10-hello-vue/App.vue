ecf6bf0df04c3e77fe51a86191e2694619fa8b6a
Xu Chunyang <xuchunyang56@gmail.com>
2024-01-10 20:01:23 +0800

    ; Fix typo

--- 01-10-hello-vue/App.vue
+++ 01-10-hello-vue/App.vue
@@ -8,7 +8,7 @@ import HelloForm from "./HelloForm.vue";
   <h1>Hello, Vue! Again!</h1>
   <p>
     可以愉快地使用 Vite 构建 MPA 了，单独的页面还可以使用
-    Vue，共享依赖不外面，但是练习用途没关系。
+    Vue，共享依赖不完美，但是练习用途没关系。
   </p>
   <div class="counters">
     <Counter />
