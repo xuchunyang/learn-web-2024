a3508735a59eadc1c9e0093b7997a410c3831fda
Xu Chunyang <xuchunyang56@gmail.com>
2024-01-23 10:15:17 +0800

    01-24-calender: init

---
+++ 01-24-calender/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from "vue";
+
+const months = ref(
+  Array.from({ length: 12 }, (_, i) => i + 1).map(
+    (i) => new Date(`2024-${i}-1`),
+  ),
+);
+</script>
+
+<template>
+  <el-row gutter="10">
+    <el-col :span="4" v-for="(_, idx) in months">
+      <el-card>
+        <template v-if="idx === 0">
+          <el-calendar
+            v-model="months[idx]"
+            :range="[new Date('2024-01-01'), new Date('2024-01-27')]"
+          ></el-calendar>
+        </template>
+        <template v-else>
+          <el-calendar v-model="months[idx]"></el-calendar>
+        </template>
+      </el-card>
+    </el-col>
+  </el-row>
+</template>
---
+++ 01-24-calender/main.js
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import ElementPlus from "element-plus";
+import zhCn from "element-plus/dist/locale/zh-cn";
+import "element-plus/dist/index.css";
+
+import App from "./App.vue";
+
+const app = createApp(App);
+
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+
+app.mount("#app");
